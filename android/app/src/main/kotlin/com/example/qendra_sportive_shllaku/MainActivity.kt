bbdf7d6e59186a8188c0cdfb3ac8794bdcd67cc4
fabianberisha <fberisha.webdev@gmail.com>
2020-04-17 20:27:53 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/qendra_sportive_shllaku/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.qendra_sportive_shllaku
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
